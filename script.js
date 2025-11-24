cb878d13ac93dec0ad374717fc0bc82d66cafb90
Syed muneerul zakireen <syedmuneerzakk@gmail.com>
2025-11-24 14:59:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
 function createTable() {
     //Write your code here
-  
+	const rows=prompt("Input number of rows");
+	const cols=prompt("Input number of columns");
+
+	if(isNaN(rows) || isNaN(cols) ||rows<=0 || cols<=0){
+		alert("Invalid input .please provide the positive number.");
+		return;
+	}
+	const rn=parseInt(rows);
+	const cn=parseInt(cols);
+
+	const table=document.getElementById("myTable");
+
+	table.innerHTML="";
+
+
+	for(let i=0;i<rn;i++){
+		const row=table.insertRow();
+		for(let j=0;j<cn;j++){
+			const cell=row.insertCell();
+			cell.textContent=`Row-${i} column-${j}`;
+		}
+	}
+
+
+
+	
+
+	
 }
